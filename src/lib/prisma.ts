caa4fc54526cb4343a3df40db8a3bcc7256d5966
Cism-Ch <coulibaly.ismael.ch.0@gmail.com>
2025-11-25 18:15:22 +0000

    feat: Add singleton Prisma client instance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@
  * Compatible avec Prisma 7
  */
 
-import { PrismaClient } from ".prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 // Singleton pattern pour éviter les multiples instances en développement
 const globalForPrisma = globalThis as unknown as {
